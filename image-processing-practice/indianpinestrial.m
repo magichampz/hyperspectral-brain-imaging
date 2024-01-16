318ab7130a3e1b463f6ba4597eb01afcd40139f0
rachaelsoh02 <84055391+rachaelsoh02@users.noreply.github.com>
2024-01-16 12:08:56 +0000

    Add files via upload

---
+++ image-processing-practice/indianpinestrial.m
@@ -0,0 +1,18 @@
+clear all; clc; clf;
+load 'Indian_pines.mat'
+
+%% displaying hyperspectral image through hypercube
+wavelength = linspace(0.4, 2.5, 220);
+hcube = hypercube(indian_pines, wavelength);
+img = colorize(hcube,'Method', 'rgb', 'ContrastStretching', true);
+imshow(img)
+
+%% pixel spectra of one pixel
+stest1 = indian_pines(1,1,:);
+stest2 = squeeze(stest1);
+figure(2)
+plot(wavelength, stest2)
+title('First Pixel Spectra')
+xlabel('Wavelength')
+ylabel('Data')
+
